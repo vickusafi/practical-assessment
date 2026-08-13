6869d30652718ed94cdef6e3c31b93d248e8925c
Victor <victorodhiambo706@gmail.com>
2026-08-13 12:42:21 +0300

    initial commit

---
+++ practical-assessment/src/main/java/com/practical_assessment/practical_assessment/PracticalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.practical_assessment.practical_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalAssessmentApplication.class, args);
+	}
+
+}
