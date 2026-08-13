799f587fdb26e39e130d6318c78ec4a340587c6d
Victor <victorodhiambo706@gmail.com>
2026-08-13 13:22:30 +0300

    add Global Exception handler for Rest Controllers

---
+++ practical-assessment/src/main/java/com/practical_assessment/practical_assessment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.practical_assessment.practical_assessment.exception;
+
+
+import com.practical_assessment.practical_assessment.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //HANDLE 404
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAccountNotFound(AccountNotFoundException ex) {
+        log.warn("Account not found: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error(ex.getMessage()));
+    }
+}
